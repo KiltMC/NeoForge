b997c368d09d7951351557c4095e186c76c8fd4b
Naz <bluspringindohq@gmail.com>
2024-02-27 21:02:53 +0800

    MISSED A $ SIGN

--- src/main/java/net/minecraftforge/client/model/geometry/BlockGeometryBakingContext.java
+++ src/main/java/net/minecraftforge/client/model/geometry/BlockGeometryBakingContext.java
@@ -61,7 +61,7 @@ public class BlockGeometryBakingContext implements IGeometryBakingContext
     @Nullable
     public IUnbakedGeometry<?> getCustomGeometry()
     {
-        return owner.parent != null && customGeometry == null ? owner.getCustomGeometry() == null ? null : new ForgeWrappedFabricUnbakedGeometry(owner.getCustomGeometry()) : (IUnbakedGeometry<?>) customGeometry;
+        return owner.parent != null && customGeometry == null ? owner.getCustomGeometry() == null ? null : new ForgeWrappedFabricUnbakedGeometry(owner.getCustomGeometry()) : customGeometry != null ? new ForgeWrappedFabricUnbakedGeometry(customGeometry) : null;
     }
 
     public void setCustomGeometry(IUnbakedGeometry<?> geometry)
