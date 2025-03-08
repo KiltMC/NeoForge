d8852d3be160454a9f125c83799f4fb5b683c922
Naz <bluspringindohq@gmail.com>
2025-03-08 11:45:05 +0800

    More patches and fixes

--- src/main/java/net/minecraftforge/registries/NamespacedDefaultedWrapper.java
+++ src/main/java/net/minecraftforge/registries/NamespacedDefaultedWrapper.java
@@ -5,31 +5,24 @@
 
 package net.minecraftforge.registries;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Stream;
-
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.core.DefaultedRegistry;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
-
-import net.minecraft.util.RandomSource;
-import org.apache.commons.lang3.Validate;
-
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.core.DefaultedRegistry;
-import com.mojang.datafixers.util.Pair;
-import com.mojang.serialization.Lifecycle;
+import net.minecraft.util.RandomSource;
+import org.apache.commons.lang3.Validate;
 import org.jetbrains.annotations.Nullable;
 import xyz.bluspring.kilt.injections.core.MappedRegistryInjection;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 class NamespacedDefaultedWrapper<T> extends DefaultedRegistry<T> implements ILockableRegistry, IHolderHelperHolder<T>, MappedRegistryInjection
 {
@@ -49,8 +42,8 @@ class NamespacedDefaultedWrapper<T> extends DefaultedRegistry<T> implements ILoc
     @Override
     public Holder<T> registerMapping(final int id, final ResourceKey<T> key, final T value, final Lifecycle lifecycle)
     {
-        if (locked)
-            throw new IllegalStateException("Can not register to a locked registry. Modder should use Forge Register methods.");
+        //if (locked)
+            //throw new IllegalStateException("Can not register to a locked registry. Modder should use Forge Register methods.");
 
         Validate.notNull(value);
         markKnown();
