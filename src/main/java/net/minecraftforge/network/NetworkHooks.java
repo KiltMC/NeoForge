dd6908c20afbd18e51dcd1b6a5f67767165d1443
Naz <bluspringindohq@gmail.com>
2025-06-10 20:45:28 +0800

    Fix Replay Mod not loading

--- src/main/java/net/minecraftforge/network/NetworkHooks.java
+++ src/main/java/net/minecraftforge/network/NetworkHooks.java
@@ -5,6 +5,34 @@
 
 package net.minecraftforge.network;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.Connection;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerContainerEvent;
+import net.minecraftforge.fml.config.ConfigTracker;
+import net.minecraftforge.fml.util.thread.EffectiveSide;
+import net.minecraftforge.network.ConnectionData.ModMismatchData;
+import net.minecraftforge.network.filters.NetworkFilters;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -12,35 +40,6 @@ import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import net.minecraft.network.chat.Component;
-import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.util.thread.EffectiveSide;
-import net.minecraftforge.network.ConnectionData.ModMismatchData;
-import net.minecraftforge.network.filters.NetworkFilters;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import io.netty.buffer.Unpooled;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.Connection;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
-import net.minecraft.server.network.ServerLoginPacketListenerImpl;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.BlockPos;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerContainerEvent;
-import net.minecraftforge.fml.config.ConfigTracker;
-import org.jetbrains.annotations.Nullable;
 
 public class NetworkHooks
 {
@@ -133,6 +132,9 @@ public class NetworkHooks
     }
 
     public static void handleClientLoginSuccess(Connection manager) {
+        // Kilt: fixes Replay Mod
+        manager.channel().attr(NetworkConstants.FML_NETVERSION).setIfAbsent(NetworkConstants.NOVERSION);
+
         if (isVanillaConnection(manager)) {
             LOGGER.info("Connected to a vanilla server. Catching up missing behaviour.");
             ConfigTracker.INSTANCE.loadDefaultServerConfigs();
