8792461bb6a2ebad1211a970220d35ecbdbc2e2d
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-06-06 19:27:51 +0200

    Clarify with comment

--- src/main/java/net/neoforged/neoforge/registries/NeoForgeRegistryCallbacks.java
+++ src/main/java/net/neoforged/neoforge/registries/NeoForgeRegistryCallbacks.java
@@ -58,8 +58,10 @@ class NeoForgeRegistryCallbacks {
             }
             addedBlocks.clear();
 
-            // Update block state ID map after each bake in case of registry changes.
+            // Kilt: Clear BLOCKSTATE_TO_ID_MAP because it might already be filled from Fabric API.
             BLOCKSTATE_TO_ID_MAP.clear();
+
+            // Update block state ID map after each bake in case of registry changes.
             for (Block block : registry) {
                 for (BlockState state : block.getStateDefinition().getPossibleStates()) {
                     BLOCKSTATE_TO_ID_MAP.add(state);
